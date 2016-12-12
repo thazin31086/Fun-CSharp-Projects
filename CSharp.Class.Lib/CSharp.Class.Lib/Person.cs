2957898ce0eba0a1b853e09a96332b73421f9e3e
Jasmine Aung <thazin31086@gmail.com>
2016-12-13 10:28:58 +1100

    Add Test Project

--- CSharp.Class.Lib/CSharp.Class.Lib/Person.cs
+++ CSharp.Class.Lib/CSharp.Class.Lib/Person.cs
@@ -1,6 +1,11 @@
-﻿namespace CSharp.Class.Lib
+﻿using System.Diagnostics;
+
+namespace CSharp.Class.Lib
 {
+    [DebuggerDisplay("Person name : {Name}, Age : {Age} years old")]
     public class PersonWithDebuggerDisplay
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 }
---
+++ CSharp.Class.Lib/CSharp.Test/PersonTest.cs
@@ -0,0 +1,19 @@
+﻿using CSharp.Class.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.Test
+{
+    [TestClass]
+    public class PersonTest
+    {
+        [TestMethod]
+        public void PersonWithDebuggerDisplayTest()
+        {
+            var p = new PersonWithDebuggerDisplay
+            {
+                Name = "Susan",
+                Age = 45
+            };           
+        }
+}
+}
